3d52296869d1dc455dbf5a8f0386020f2e6d5062
foxyy <foxyy@foxyydeMacBook-Air.local>
2022-10-06 10:56:54 +0800

    wrong

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,16 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+console.log('Hello World from Webpacker')
+import "controllers";
+
+import "scripts/shared";
+import "scripts/frontend";
+
+import "styles/shared";
+import "styles/frontend";
+
+import "controllers"
